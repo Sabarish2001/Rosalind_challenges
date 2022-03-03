79677223c398a501560cf930528eb63473dcf69a
Sabarish2001 <76895003+Sabarish2001@users.noreply.github.com>
2022-03-03 09:17:10 +0530

    Create pattern_occurences.py
    Find All Occurrences of a Pattern in a String

---
+++ pattern_occurences.py
@@ -0,0 +1,15 @@
+def pattern_occurences(text,pattern):
+    text_len = len(text)
+    pattern_len = len(pattern)
+    occurences = []
+    for i in range(text_len):
+        if text[i : i + pattern_len] == pattern:
+            occurences.append(i)
+    return occurences
+
+genome = input("")
+pattern = input("")
+
+res = pattern_occurences(genome,pattern)
+for i in res:
+    print(i,end=" ")
